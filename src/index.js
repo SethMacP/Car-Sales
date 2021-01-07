338d48c121ff689e42b791be9e4d9d02fe3eab8e
SethMacP <macpherson.seth@gmail.com>
2021-01-07 13:46:56 -0800

    Store created and provider setup

--- src/index.js
+++ src/index.js
@@ -5,5 +5,20 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import {Provider} from 'react-redux'
+import {applyMiddleware, createStore} from 'redux'
+import {logger} from 'redux-logger'
+import thunk from 'redux-thunk'
+
+const store = createStore(reducer,applyMiddleware(thunk, logger) );
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+
+ReactDOM.render(
+
+<Provider store={store}>
+<App />
+</Provider>
+
+, rootElement);
